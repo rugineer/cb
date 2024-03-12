71b149464dd9b3a067a0e7d4cb0aa58b127479e6
igiant <sergey.grishayev@gmail.com>
2024-03-12 11:25:04 +0400

    v0.1.0

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+use std::path::Path;
+use std::sync::mpsc::channel;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
+fn main() {
+    let mut ctx = ClipboardContext::new().expect("clipboard context error");
+    let mut args = std::env::args();
+    let param = args.nth(1)
+        .unwrap_or(String::from(""))
+        .to_lowercase();
+    if param.eq("-p") {
+        println!(
+            "{}",
+            ctx.get_contents()
+                .unwrap_or(String::from("clipboard error"))
+        );
+        return;
+    }
+    let (tx, cx) = channel::<String>();
+    spawn(move || {
+        let mut buffer = Vec::new();
+        let mut stdin = io::stdin();
+        stdin.read_to_end(&mut buffer).expect("read error");
+        let str = String::from_utf8_lossy(&buffer).to_string();
+        tx.send(str).unwrap()
+    });
+    sleep(Duration::from_millis(100));
+    let mut str = cx.try_recv().unwrap_or(String::from(""));
+    if str.eq("") {
+        sleep(Duration::from_millis(500));
+        str = cx.try_recv().unwrap_or(String::from(""));
+        if str.eq("") {
+            return;
+        }
+    }
+    if param.eq("-v") {
+        println!("{}", &str)
+    } else if param.ne("") {
+        File::create(Path::new(&param))
+            .expect(&format!("file {} create error", &param))
+            .write_all(str.as_bytes())
+            .expect(&format!("write to file '{}' error", &param));
+    }
+    ctx.set_contents(str).expect("clipboard error");
+}
+
